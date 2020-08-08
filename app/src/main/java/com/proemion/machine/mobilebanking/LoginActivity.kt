504d1db483521a1341df5fa6a3120f56b4e9898f
Chukwuebuka Ezelu <33615617+zenas91@users.noreply.github.com>
2020-08-08 14:05:29 +0200

    Update

--- app/src/main/java/com/proemion/machine/mobilebanking/LoginActivity.kt
+++ app/src/main/java/com/proemion/machine/mobilebanking/LoginActivity.kt
@@ -5,12 +5,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 
     fun clickRegisterLayout(view: View?) {
@@ -20,6 +20,11 @@ class LoginActivity : AppCompatActivity() {
         val options: ActivityOptions =
             ActivityOptions.makeSceneTransitionAnimation(this, fab, fab.transitionName)
         startActivity(Intent(this, RegisterActivity::class.java), options.toBundle())
+    }
+
+    fun onClickLogin(view: View?){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 
 }
